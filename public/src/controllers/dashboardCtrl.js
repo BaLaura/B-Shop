ea10cb370bb2ab82024881216ff29d5fad262fc1
dellia16 <robert.osvath@barandisolutions.com>
2015-08-26 13:03:32 +0300

    Now users remain logged in; Logout button implemented

--- public/src/controllers/dashboardCtrl.js
+++ public/src/controllers/dashboardCtrl.js
@@ -1,17 +1,18 @@
 angular.module('b-shop')
-	.controller('dashboardCtrl', function($scope,$location,$rootScope){
+	.controller('dashboardCtrl', function($scope,$location,$rootScope,loginservice){
 		var visibility = function (){
 			if ($rootScope.logged){
 				return false;
 			} else {
 				return true;
 			}
-
+		}
+		$scope.logout = function(){
+			loginservice.removeItem('user');
+			$rootScope.loggedInUser = null;
+	    	$location.path('/')
 		}
 		if ($rootScope.logged){
 			console.log('Logged in!');
-		} else {
-			$location.path('/');
-			window.alert('You must log in!')
 		}
 	});
--- public/src/controllers/loginCtrl.js
+++ public/src/controllers/loginCtrl.js
@@ -1,19 +1,32 @@
 angular.module('b-shop')
-    .controller('loginCtrl', function($scope, $rootScope, $location, userservice) {
+    .controller('loginCtrl', function($scope, $rootScope, $location, userservice,loginservice) {
     	$rootScope.logged = false;
-    	$scope.loginEvent = function () { 
-    		userservice.login($scope.eMail,$scope.password).success(function(data){
-    			console.log(data);
+    	if ((loginservice.getItem('email') == null)&&(loginservice.getItem('password')==null)){
+
+	    	$scope.loginEvent = function () { 
+	    		userservice.login($scope.eMail,$scope.password).success(function(data){
+	    			console.log(data);
+	    			loginservice.setItem('user', data);
+	    			if (data != null){
+	    				$rootScope.loggedInUser = data;
+	    				$rootScope.logged = true;
+	    				window.alert('Success');
+	    				$location.path('/dash');
+	    			} else {
+	    				window.alert('Wrong email or passowrd! Try again!');
+	    			}
+
+	    		});
+
+	    	}    	
+    	} else {
+    		userservice.login(loginservice.getItem('email'),loginservice.getItem('password')).success(function(data){
     			if (data != null){
-    				$rootScope.loggedInUser = data;
+    				$rootScope.loggedInUser=data;
     				$rootScope.logged = true;
-    				window.alert('Success');
+    				window.alert('Welcome back!');
     				$location.path('/dash');
-    			} else {
-    				window.alert('Wrong email or passowrd! Try again!');
     			}
-
     		});
-
-    	}    	
+    	}
     });
